dd92ba5867a5ea191fd8e22e4e9dfab442ab3270
tounkane <tounkane@gmail.com>
2024-10-09 22:42:20 +0200

    Delete dating-lubien-main directory

--- dating-lubien-main/src/functions/getStepById.ts
+++
@@ -1,9 +0,0 @@
-import { Step, Story } from "@/types/Step";
-
-export function getStepById(id: string, story: Story): Step {
-   if (typeof id !== "string") throw Error("Id is not a string");
-   if (Array.isArray(story)) throw Error("Story is not an array");
-
-   const step = story.chapitres.find((step: Step) => step.identifiant === id);
-   return step
-} 
